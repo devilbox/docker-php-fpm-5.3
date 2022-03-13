74ca77393c911304b0870c53cc990d7dd8542491
cytopia <cytopia@everythingcli.org>
2022-03-13 12:39:50 +0100

    Unify workflows

--- tests/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-set -o pipefail
-
-IMAGE="${1}"
-ARCH="${2}"
-
-docker run --rm --platform "${ARCH}" --entrypoint=php "${IMAGE}" -v | grep -E '^PHP 5\.3'
-docker run --rm --platform "${ARCH}" --entrypoint=php-fpm "${IMAGE}" -v | grep -E '^PHP 5\.3'
